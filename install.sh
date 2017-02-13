340d688127a37a2d069ead3266a7d8dc89768f7f
Rahul Revo <rahulrevo@gmail.com>
2017-02-12 17:23:50 -0800

    Emacs and lein configuration
    Emacs started configuration from
    http://www.braveclojure.com/basic-emacs/

--- install.sh
+++ install.sh
@@ -5,7 +5,22 @@ dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Link the dotfiles
 files="gitignore_global vimrc"
+dirs="emacs.d lein"
+
 for f in $files; do
-    ln -s "$dir/$f" ~/."$f"
+    if [ -e ~/."$f" ]
+    then
+        echo "~/.$f exists"
+    else 
+        ln -sv "$dir/$f" ~/."$f"
+    fi
+done
+for d in $dirs; do
+    if [ -e ~/."$d" ]
+    then
+        echo "~/.$d exists"
+    else 
+        ln -sv "$dir/$d/" ~/."$d"
+    fi
 done
 
